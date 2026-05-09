aff2e26482f3e08c29bd7c54fee5ca8baf5f0422
birezn <birezn@outlook.com>
2026-05-09 10:58:15 +0800

    安装证书

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
-apt-get install -y --no-install-recommends curl git
+apt-get install -y ca-certificates curl git
 
 # cleanup
 rm -rf /var/lib/apt/lists/*
