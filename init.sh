3ce7e1f2447516724bab693923231525823b2592
birezn <birezn@outlook.com>
2026-05-16 23:38:21 +0800

    添加依赖 jq dnsutils bc procps

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
-apt-get install -y ca-certificates curl git
+apt-get install -y ca-certificates curl git jq dnsutils bc procps
 
 # cleanup
 rm -rf /var/lib/apt/lists/*
