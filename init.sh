0c7e2ce503923b778b0254d349d50e84489e610e
birezn <birezn@outlook.com>
2026-05-17 17:10:21 +0800

    添加依赖 dmidecode lm-sensors pciutils smartmontools fio sysbench iproute2 imagemagick mtr-tiny iperf3 stun

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
-apt-get install -y ca-certificates curl git jq dnsutils bc procps
+apt-get install -y ca-certificates curl git jq dnsutils bc procps dmidecode lm-sensors pciutils smartmontools fio sysbench iproute2 imagemagick mtr-tiny iperf3 stun
 
 # cleanup
 rm -rf /var/lib/apt/lists/*
